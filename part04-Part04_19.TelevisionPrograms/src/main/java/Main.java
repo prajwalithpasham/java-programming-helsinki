2982da7e7ad1c25f9fc36913536bac32dbfbf65f
prajwalithpasham <prajwalithpasham@gmail.com>
2026-09-02 12:17:47 +0530

    Part-04 19 problem solved

--- part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
+++ part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
@@ -9,5 +9,26 @@ public class Main {
         ArrayList<TelevisionProgram> programs = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+
+            String name = scanner.nextLine();
+            if (name.isEmpty()) {
+                break;
+            }
+
+            int duration = Integer.valueOf(scanner.nextLine());
+
+            programs.add(new TelevisionProgram(name, duration));
+        }
+
+        System.out.println("Maximum duration: ");
+        int MaxD = scanner.nextInt();
+
+
+        for (TelevisionProgram program : programs) {
+            if(program.getDuration() <= MaxD) {
+                System.out.println(program);
+            }
+        }
     }
 }
