567d84842da91bb75d8b0f480345846c514d1fd1
prajwalithpasham <prajwalithpasham@gmail.com>
2026-09-02 12:55:18 +0530

    solved part 04 - 22 prob

--- part04-Part04_22.Cubes/src/main/java/Cubes.java
+++ part04-Part04_22.Cubes/src/main/java/Cubes.java
@@ -6,5 +6,18 @@ public class Cubes {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String number = "0";
+
+        while(true){
+
+            number = scanner.nextLine();
+
+            if (number.equals("end")) {
+                break;
+            }
+
+            System.out.println(Integer.valueOf(number) * Integer.valueOf(number) * Integer.valueOf(number));
+        }
+        
     }
 }
