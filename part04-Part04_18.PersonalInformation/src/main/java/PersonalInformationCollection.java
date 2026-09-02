988245e18f57f813e31c934b3c968634a0dce631
prajwalithpasham <prajwalithpasham@gmail.com>
2026-09-02 10:29:41 +0530

    Part-04 18 excercise completed

--- part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
+++ part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
@@ -10,5 +10,25 @@ public class PersonalInformationCollection {
         ArrayList<PersonalInformation> infoCollection = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+
+            String firstName = scanner.nextLine();
+            
+            if (firstName.isEmpty()) {
+                break;
+            }
+
+            String lastName = scanner.nextLine();
+
+            String idNumber = scanner.nextLine();
+
+            infoCollection.add(new PersonalInformation(firstName, lastName, idNumber));
+        }
+
+        for(PersonalInformation info : infoCollection) {
+            System.out.println(info.getFirstName() + " " + info.getLastName());
+        }
+        
+
     }
 }
