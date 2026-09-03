7a9f44cf363fc07c970d7a0359da8ee5ce216ec5
prajwalithpasham <prajwalithpasham@gmail.com>
2026-09-03 18:31:18 +0530

    Solved part 04-25

--- part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
+++ part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
@@ -7,5 +7,21 @@ public class PrintingASpecifiedFile {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String filename;
+
+        filename = scanner.nextLine();
+        findfile(filename);
+
+    }
+
+    public static void findfile(String filename) {
+        try (Scanner fileScanner = new Scanner(Paths.get(filename))) {
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
