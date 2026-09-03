0b80455d7d8747c4fa4ca4dd6694eee08cafaa52
prajwalithpasham <prajwalithpasham@gmail.com>
2026-09-03 17:27:52 +0530

    part 04 - 24 prob solved

--- part04-Part04_24.PrintingAFile/src/main/java/PrintingAFile.java
+++ part04-Part04_24.PrintingAFile/src/main/java/PrintingAFile.java
@@ -6,5 +6,16 @@ public class PrintingAFile {
 
     public static void main(String[] args) {
 
+        try (Scanner scanner = new Scanner(Paths.get("data.txt"))) {
+
+            while (scanner.hasNextLine()) {
+                // we read one line
+                String row = scanner.nextLine();
+                // we print the line that we read
+                System.out.println(row);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
