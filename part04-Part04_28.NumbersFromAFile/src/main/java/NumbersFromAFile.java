addf0a0fbe3143853f54f971e1d4e891b7d7eaf1
prajwalithpasham <prajwalithpasham@gmail.com>
2026-09-04 09:26:12 +0530

    Solved Part_04-28

--- part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
+++ part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class NumbersFromAFile {
 
     public static void main(String[] args) {
+        int count = 0;
         Scanner scanner = new Scanner(System.in);
+
 
         System.out.print("File? ");
         String file = scanner.nextLine();
@@ -14,6 +16,19 @@ public class NumbersFromAFile {
         System.out.print("Upper bound? ");
         int upperBound = Integer.valueOf(scanner.nextLine());
 
+        try(Scanner fileInt = new Scanner(Paths.get(file))){
+
+            while(fileInt.hasNextLine()){
+                int number = Integer.valueOf(fileInt.nextLine());
+                if(number >= lowerBound && number <= upperBound){
+                    count++;
+                }
+            }
+            System.out.println("Numbers: " + count);
+
+        } catch (Exception e){
+            System.out.println("Reading the file " + file + " failed.");
+        }
     }
 
 }
